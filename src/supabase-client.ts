64f976bb21380d44884418b5dbde52593428bd71
henhhenh13 <henhhenh13@gmail.com>
2024-12-01 23:51:45 +0700

    feat: update env

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yygawohhkenacgqzqkgf.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5Z2F3b2hoa2VuYWNncXpxa2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1OTUyNTgsImV4cCI6MjA0NzE3MTI1OH0.42eN_qazH-2NE6B7Z--VBoplHdoCroTwkUYVTZUl66Y';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
